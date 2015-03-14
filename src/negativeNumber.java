33ba5e4f42bdf24865ec7bf19c178d179c03be92
Ishan Arya <ishan.arya130@gmail.com>
2015-03-14 12:38:30 -0400

    Start of Game

---
+++ src/negativeNumber.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Ishan on 3/14/2015.
+ */
+
+import java.util.Scanner;
+public class negativeNumber {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+        System.out.println("Put in your first number.");
+        int check = sc.nextInt();
+
+    }
+}
