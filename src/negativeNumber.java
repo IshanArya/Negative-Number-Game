957bb8f15f461b0d4950bb2adb53b41b40eeeb04
Ishan Arya <ishan.arya130@gmail.com>
2015-03-21 10:44:58 -0400

    Finished program.
    Tells your lowest nonnegative number after many inputs.

--- src/negativeNumber.java
+++ src/negativeNumber.java
@@ -8,8 +8,19 @@ public class negativeNumber {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args){
-        System.out.println("Put in your first number.");
-        int check = sc.nextInt();
+        System.out.println("Put in your first number: ");
+        int input = sc.nextInt();
+        int check = input;
 
+        while (input > 0){
+            if(input < check){
+                check = input;
+            }
+
+            System.out.println("Put in your next number: ");
+            input = sc.nextInt();
+        }
+
+        System.out.println(check);
     }
 }
